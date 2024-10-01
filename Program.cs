b88126c43661a29c3f7b84958d251e59854a8647
hoang <=>
2024-10-01 07:52:33 +0700

    lamda expression

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace DemoLambdaExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "David", "Jane", "Peter", "John", "Mark" };
+            foreach (string item in names.OrderBy(s => s))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
